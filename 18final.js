8cc2078fde24ed144f030545d5a7ca1a6345a565
Szabo Rihcard <ignocius@gmail.com>
2017-02-17 09:53:25 +0100

    logs commented out

--- 18final.js
+++ 18final.js
@@ -7,8 +7,8 @@ function giveMeBackTheSameLetters(input, input2) {
     for (var j = 0; j < input2.length; j++) {
       // console.log(input2[j]);
       if (input[i] === input2[j]) {
-        console.log('inp1' + input[i]);
-        console.log('inp2' + input2[j]);
+        // console.log('inp1' + input[i]);
+        // console.log('inp2' + input2[j]);
          commonCharArray.push(input2[j]);
       }
     }
@@ -18,4 +18,3 @@ function giveMeBackTheSameLetters(input, input2) {
 
 console.log(giveMeBackTheSameLetters('car', 'bar'));
 console.log(giveMeBackTheSameLetters('war', 'thor'));
-// console.log(giveMeBackTheSameLetters('apple', 'map'));
