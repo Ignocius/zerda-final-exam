1c72b5f04dba326dc379fe046c5d8413c9810235
Szabo Rihcard <ignocius@gmail.com>
2017-02-17 09:54:56 +0100

    logs deleted

--- 18final.js
+++ 18final.js
@@ -5,10 +5,7 @@ function giveMeBackTheSameLetters(input, input2) {
   for (var i = 0; i < input.length; i++) {
     console.log();
     for (var j = 0; j < input2.length; j++) {
-      // console.log(input2[j]);
       if (input[i] === input2[j]) {
-        // console.log('inp1' + input[i]);
-        // console.log('inp2' + input2[j]);
          commonCharArray.push(input2[j]);
       }
     }
